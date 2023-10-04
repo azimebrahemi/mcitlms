26455ace680351bbe8d908be768bb0aaf488818b
azimebrahemi <azimebrahemi@gmail.com>
2023-10-04 12:12:35 +0430

    Models Create for User and Letter

---
+++ src/main/java/com/lms/lms/model/OutgointLetter.java
@@ -0,0 +1,99 @@
+package com.lms.lms.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+public class OutgointLetter {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String file_name;
+    private String file_path;
+    private String source;
+    @Basic(optional = false)
+    @Column(name = "LastTouched", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDateTime;
+    private String content;
+    private String status;
+    private boolean is_approved;
+    private String uploadedby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFile_name() {
+        return file_name;
+    }
+
+    public void setFile_name(String file_name) {
+        this.file_name = file_name;
+    }
+
+    public String getFile_path() {
+        return file_path;
+    }
+
+    public void setFile_path(String file_path) {
+        this.file_path = file_path;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Date getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(Date creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isIs_approved() {
+        return is_approved;
+    }
+
+    public void setIs_approved(boolean is_approved) {
+        this.is_approved = is_approved;
+    }
+
+    public String getUploadedby() {
+        return uploadedby;
+    }
+
+    public void setUploadedby(String uploadedby) {
+        this.uploadedby = uploadedby;
+    }
+}
---
+++ src/main/java/com/lms/lms/model/User.java
@@ -0,0 +1,57 @@
+package com.lms.lms.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String name;
+    private String pass;
+    private String role;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
